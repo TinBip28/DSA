9f83114df8033e87c3e315e1c53bca690db900c4
QuangTin1-hus <lequangtin_t67@hus.edu.vn>
2023-09-24 00:35:47 +0700

    23/09/2023

--- src/week2/sortstrategy/App.java
+++ src/week2/sortstrategy/App.java
@@ -1,33 +1,19 @@
 package week2.sortstrategy;
 
-import java.util.Arrays;
-import java.util.Random;
+import javax.swing.*;
 
 public class App {
-    private static int[] randomIntArray() {
-        Random random = new Random();
-        int n = random.nextInt(1000);
-        int[] data = new int[n];
-        for (int i = 0; i < n; i++) {
-            data[i] = random.nextInt(0, 100);
-        }
-        return data;
-    }
-
-    private static String ArrayToString(int[] data) {
-        return Arrays.toString(data).replaceAll(",", "");
-    }
 
     public static void main(String[] args) {
-        int[] data = randomIntArray();
-        SortStrategy sortStrategy = SortStrategy.getInstance();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
 
-        ISort quickSort = new QuickSort();
-        sortStrategy.setSortee(quickSort);
-        sortStrategy.sort(data);
-        System.out.println(ArrayToString(data));
-        System.out.println(quickSort.getTimes());
-        System.out.println(quickSort.getCompareNums());
-        System.out.println(quickSort.getSwapCount());
+        Controller comparisonChart = new Controller();
+        window.add(comparisonChart);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
