9ebae6189dd35025e72b20ce6877d5a3d3cdd265
QuangTin1-hus <lequangtin_t67@hus.edu.vn>
2023-11-03 15:16:39 +0700

    3/11/2023

--- src/week5/codelearn/Ex62.java
+++ src/week5/codelearn/Ex62.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Ex62 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] array = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            array[i] = input.nextInt();
         }
 
         BinaryTree bt = new BinaryTree();
         for (int i = 0; i < n; i++) {
-            bt.addNode(arr[i]);
+            bt.addNode(array[i]);
         }
         bt.traverseInOrder(bt.root);
     }
